deb296fce68648330b0122be2bf048d3710da4ad
Teruaki0903 <teruaki.murakami0903@gmail.com>
2024-06-20 18:35:51 +0900

    ADD 初期設定&基礎実装

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
+
+
+Route::get('/posts', [PostController::class, 'index']);   
 
 Route::get('/', function () {
     return view('welcome');
